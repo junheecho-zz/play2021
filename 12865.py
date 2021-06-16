384d91eeb82be1238641574e5b17665491894233
lovelyamuro <junhee.cho@lge.com>
2021-06-16 23:39:04 +0900

    update 12865

--- 12865.py
+++ 12865.py
@@ -1,5 +1,66 @@
-# 12865 평범한배낭
+ # 12865 평범한배낭
 
+def f(k, items):
+    """Returns the sum of value of all the items in the bag which can take up to K weight.
+    
+    Parameters
+    ----------
+    k : int. 버틸 수 있는 최대 무게
+    items: [w:int, v:int]: w 각 물건의 무게, v 물건의 가치, 1 <= w <= 100000, 0 <= v <= 1000
+    
+    Returns
+    -------
+    value : int. 배낭에 넣은 물건의 가치의 최대 합
+    
+    Example
+    -------
+    >>> k = 7
+    >>> items = [[6,13], [4,8], [3,6], [5,12]]
+    >>> f(k, items)
+    14
+    """
+    print ('items', items)
+    dp = [0] * (k+1)
+    for i in range(k+1):
+        for w, v in items:
+            y = 0
+            if i - w >= 0 and w + i <= k:
+                y = dp[i-w] + v
+                print (f'i={i}, [{w},{v}]: {y} = dp[{i}-{w}] + {v} ')
+            dp[i] = max (y, dp[i])
+
+    print ('dp', dp)
+    print ('ret', dp[k]) 
+    return dp[k]
+
+assert f(7, [[9, 2], [2, 1], [8, 9]]) == 1
+# case 1. take O X X
+# case 2. take X O X
+# case 3. take X X O
+# case 4. take O O X
+# case 5. take X O O
+# case 6. take O O O
+#assert f(7, [[7, 1], [8, 0], [9, 9]]) == 1
+assert f(7, [[9, 2], [2, 1], [8, 9]]) == 1
+assert f(7, [[9, 3], [9, 2], [6, 1]]) == 1
+assert f(7, [[3, 3], [4, 5], [3, 2]]) == 8
+assert f(7, [[3, 7], [4, 9], [3, 8]]) == 17
+assert f(7, [[2, 2], [2, 1], [3, 9]]) == 12
+
+assert f(7, [
+    [6,13],
+    [4,8],
+    [3,6],
+    [5,12]
+]) == 14
+
+def get_ints(): return [int(v) for v in input().strip().split()]
+
+if __name__ == '__main__':
+    N, K = get_ints()
+    items = [get_ints() for _ in range(N)]
+    print (f(K, items))
+        
 def f(k, items):
     """Returns the sum of value of all the items in the bag which can take up to K weight.
     
@@ -35,18 +96,3 @@ def f(k, items):
     ret = max(case1, case2)
     
     return ret
-
-assert f(7, [
-    [6,13],
-    [4,8],
-    [3,6],
-    [5,12]
-]) == 14
-
-def get_ints(): return [int(v) for v in input().strip().split()]
-
-if __name__ == '__main__':
-    N, K = get_ints()
-    items = [get_ints() for _ in range(N)]
-    print (f(K, items))
-        

8c355be840174993849af8f2cd064ad537a9de87
junheecho-zz <lovelyamuro@naver.com>
2021-06-24 23:21:15 +0900

    fix 32.py

--- leetcode/32.py
+++ leetcode/32.py
@@ -2,34 +2,7 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         def f(s):
-            def extract_parentheses(s):
-                d = 0
-                at = 0
-                parentheses = 0
-                valid = True
-
-                if s=='': return False, 0, ''
-
-                for i, c in enumerate(s):
-                    if c=='(': 
-                        d += 1
-                    if c==')' and d > 0: 
-                        d -= 1
-                        parentheses += 2
-                    if c==')' and d == 0:
-                        valid = False
-                        break
-
-                return valid and d==0, parentheses, s[i+1:]
-
-            valid, parentheses, s1 = extract_parentheses(s)
-            print (valid, parentheses, s1)
-            if valid:
-                return parentheses + f(s1)
-            else:
-                rest = 0 if s1 == '' else f(s1)
-                return max(parentheses, rest)
-
+            pass
         ret = f(s)
         print(s, '=>', ret)
         return ret
